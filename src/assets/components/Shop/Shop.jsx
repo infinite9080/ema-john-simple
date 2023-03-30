5653239558bd402fc29e80bd2ec69af1bb4d617d
infinite9080 <tanzilkarim12@gmail.com>
2023-03-31 00:41:20 +0600

    finished-module

--- src/assets/components/Shop/Shop.jsx
+++ src/assets/components/Shop/Shop.jsx
@@ -4,21 +4,32 @@ import "./Shop.css";
 
 const Shop = () => {
   const [products, setProduct] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("../../../../public/products.json")
       .then((res) => res.json())
       .then((data) => setProduct(data));
   }, []);
+  const handleAddToCart = (product) => {
+    // cart.push(product);
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
   return (
     <div className="shop-container">
       <div className="products-container">
         {products.map((product) => (
-          <Product key={product.id} product={product}></Product>
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
         ))}
       </div>
       <div className="cart-container">
         <h4>Order Summary</h4>
+        <p>Selected Items: {cart.length}</p>
       </div>
     </div>
   );

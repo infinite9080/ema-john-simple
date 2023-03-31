11a1371196b794afff3cdb0e799ef6a81e3a30d5
infinite9080 <tanzilkarim12@gmail.com>
2023-03-31 12:42:34 +0600

    cart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./assets/components/Header/Header";
 import Shop from "./assets/components/Shop/Shop";
